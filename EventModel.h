f79b66e8764c76357e7c9b9ab0e84e2fbfe4d736
Prasann Shah <preachx@gmail.com>
2015-02-04 17:50:00 +0530

    added event model, title

---
+++ EventModel.h
@@ -0,0 +1,13 @@
+//
+//  EventModel.h
+//  JSON_HelloWorld
+//
+//  Created by Prasann Shah on 04/02/15.
+//  Copyright (c) 2015 Underplot ltd. All rights reserved.
+//
+
+#ifndef JSON_HelloWorld_EventModel_h
+#define JSON_HelloWorld_EventModel_h
+
+
+#endif
---
+++ EventModel.m
@@ -0,0 +1,9 @@
+//
+//  EventModel.m
+//  JSON_HelloWorld
+//
+//  Created by Prasann Shah on 04/02/15.
+//  Copyright (c) 2015 Underplot ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
